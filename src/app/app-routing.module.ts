281061ecf345dd0f2221ed7a88f666395ac14a14
pooja1422 <k.puja14201@gmail.com>
2021-04-08 17:58:17 +0530

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { NewUserComponent } from './new-user/new-user.component';
 
@@ -8,6 +9,7 @@ import { NewUserComponent } from './new-user/new-user.component';
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'newUser', component: NewUserComponent },
+  { path: 'dashboard', component: DashboardComponent},
   
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NewUserComponent } from './new-user/new-user.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NewUserComponent
+    NewUserComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
